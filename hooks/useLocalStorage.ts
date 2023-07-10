3564c72e65773d048a9cb8f420d93ef554687e1d
dragonsea0927 <dragonsea927@proton.me>
2023-07-10 17:15:49 -0400

    update useLocalStorage

--- hooks/useLocalStorage.ts
+++ hooks/useLocalStorage.ts
@@ -6,6 +6,12 @@ import {
     useState,
 } from 'react';
 
+declare global {
+    interface WindowEventMap {
+        'local-storage': CustomEvent;
+    }
+}
+
 type SetValue<T> = Dispatch<SetStateAction<T>>;
 
 export default function useLocalStorage<T>(
@@ -53,6 +59,9 @@ export default function useLocalStorage<T>(
             window.localStorage.setItem(key, JSON.stringify(newValue));
             /* Save state */
             setStoredValue(newValue);
+
+            /* We dispatch a custom event so every useLocalStorage hook are notified */
+            window.dispatchEvent(new Event('local-storage'));
         } catch (error) {
             console.warn(`Error setting localStorage key "${key}":`, error);
         }
@@ -61,13 +70,42 @@ export default function useLocalStorage<T>(
     useEffect(() => {
         setStoredValue(readValue());
         // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []); /* Empty array ensures that effect is only run on mount */
+    }, []);
+
+    const handleStorageChange = useCallback(
+        (event: StorageEvent | CustomEvent) => {
+            if (
+                (event as StorageEvent)?.key &&
+                (event as StorageEvent).key !== key
+            ) {
+                return;
+            }
+            setStoredValue(readValue());
+        },
+        [key, readValue],
+    );
+
+    useEffect(() => {
+        /* This only works for other documents, not the current one */
+        window.addEventListener('storage', handleStorageChange);
+
+        /* This is a custom event, triggered in writeValueToLocalStorage */
+        window.addEventListener('local-storage', handleStorageChange);
+
+        /* Remove event listeners on cleanup */
+        return () => {
+            window.removeEventListener('storage', handleStorageChange);
+            window.removeEventListener('local-storage', handleStorageChange);
+        };
+
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
 
     return [storedValue, setValue];
 }
 
 /**
- * A wrapper for "JSON.parse()"" to support "undefined" value
+ * A wrapper for "JSON.parse()" to support "undefined" value
  */
 function parseJSON<T>(value: string | null): T | undefined {
     try {
