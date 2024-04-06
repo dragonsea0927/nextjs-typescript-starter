4eb3b2adee00d780f15c5a8bb571122001a7d079
dragonsea0927 <dragonsea927@proton.me>
2024-04-06 12:16:40 -0400

    update frontpage - remove sessionStorage example

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,6 @@
 import BasicHeader from '@/components/BasicHeader';
-import { useSessionStorage } from '@/hooks/useSessionStorage';
 
 export default function Home() {
-    const [value, setValue, removeValue] = useSessionStorage('test-key', 0);
-
     return (
         <>
             <BasicHeader
@@ -16,30 +13,6 @@ export default function Home() {
                     className: 'c-btn',
                 }}
             />
-            <div>
-                <p>Count: {value}</p>
-                <button
-                    onClick={() => {
-                        setValue((x: number) => x + 1);
-                    }}
-                >
-                    Increment
-                </button>
-                <button
-                    onClick={() => {
-                        setValue((x: number) => x - 1);
-                    }}
-                >
-                    Decrement
-                </button>
-                <button
-                    onClick={() => {
-                        removeValue();
-                    }}
-                >
-                    Reset
-                </button>
-            </div>
         </>
     );
 }
