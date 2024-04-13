a22da34cddd33ff9ff6d8326d74c20227a1e8282
dragonsea0927 <dragonsea927@proton.me>
2024-04-13 18:04:02 -0400

    add og-image

--- components/MetaData.tsx
+++ components/MetaData.tsx
@@ -7,7 +7,7 @@ export default function MetaData({ ...customMeta }: MetaDataProps) {
     const meta: MetaDataProps = {
         title: 'Next.js typescript starter',
         description:
-            'A Next.js starter that includes a collection of reusable components, hooks, and utilities to build amazing projects with complex animations and page transitions using GSAP.',
+            'A Next.js typescript starter that includes a collection of reusable components, hooks, and utilities to build amazing projects with complex animations and page transitions using GSAP.',
         image: `${process.env.NEXT_PUBLIC_BASE_URL}/static/og-image.png`,
         type: 'website',
         ...customMeta,
